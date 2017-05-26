061f622ee2da87ed7492489e7cb4e6909a764a36
David Valpey <greengargoyle@hotmail.com>
2017-05-25 23:33:44 -0700

    Add function to convert JSON files into DataFrame

---
+++ src/functions.py
@@ -0,0 +1,30 @@
+import json
+from pandas.io.json import json_normalize
+
+def import_json_to_df(path):
+    """
+    Import file with list of JSON into a DataFrame.
+    
+    INPUT
+    path : string
+        Path to JSON file
+
+    OUTPUT
+    json_list : DataFrame
+        'json_normalize' flattens the nested dicts in the json into new columns 
+        and turns everything into one DataFrame object.
+
+    """
+    json_list = []
+    
+    with open(path, 'r') as f:
+        for line in f:
+            json_line = json.loads(line)
+
+            # '__reference' is Datalinq internal and can be ignored (only in Facebook data)
+            json_line.pop('__reference', None)
+
+            json_list.append(json_line)
+            
+    return json_normalize(json_list, sep='_')
+
